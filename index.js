06801e00d87eeb279dd49dd2827ba58b51cbea63
SteakFisher <88953813+SteakFisher@users.noreply.github.com>
2022-07-08 18:28:08 +0400

    Flawed update for detecting automod mutes

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ function sendLogEmbed(executor, user, eventTitle, guild, reason){
   .setDescription(`**Target** \n ${user.tag} [${user.id}] \n \n **User**\n ${executor.tag} [${executor.id}] \n\n **Reason**\n ${reason}`)
   .setTimestamp()
   .setFooter({text: "aSpicyModerator"})
-  .setThumbnail(executor.avatarURL({format: "png"}))
+  if(executor.avatarURL){
+    embed.setThumbnail(executor.avatarURL({format: "png"}))
+  }
 
   let logChannel = guild.channels.cache.get(config.logChannel)
   logChannel.send({ embeds: [embed] })
@@ -50,6 +52,10 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
+client.on("message", async message => {
+  if(message.channel.type === "dm") return
+  if(message.channel.id === "969948890616975360"){console.log(message)} 
+})
 
 client.on("guildMemberUpdate", async (oldMember, newMember) =>{
   if(newMember.user.bot){return}
@@ -57,24 +63,30 @@ client.on("guildMemberUpdate", async (oldMember, newMember) =>{
 		limit: 1,
 		type: "MEMBER_UPDATE",
 	})
-  if(oldFetchedLogs === {}){oldFetchedLogs = fetchedLogs}
+  if(oldFetchedLogs === {}){
+    oldFetchedLogs = fetchedLogs
+    return
+  }
   if(JSON.stringify(oldFetchedLogs) === JSON.stringify(fetchedLogs)){return}
   if(fetchedLogs.entries.first().changes[0].key == "communication_disabled_until"){
     let reason = "No reason"
     let firstEntry = fetchedLogs.entries.first()
     if(firstEntry.reason){reason = firstEntry.reason}
 
-    if((fetchedLogs.entries.first().changes[0].old) && !(fetchedLogs.entries.first().changes[0].new)){
+    if((fetchedLogs.entries.first().changes[0].old) && !(fetchedLogs.entries.first().changes[0].new) && (fetchedLogs.entries.executor)){
       sendLogEmbed(firstEntry.executor, firstEntry.target, "Member unmute", newMember.guild, reason)
       newMember.send("You have been unmuted")
     }
-    else if((fetchedLogs.entries.first().changes[0].new) && !(fetchedLogs.entries.first().changes[0].old)){
+    else if((fetchedLogs.entries.first().changes[0].new) && !(fetchedLogs.entries.first().changes[0].old) && (fetchedLogs.entries.executor)){
       sendLogEmbed(firstEntry.executor, firstEntry.target, "Member mute", newMember.guild, reason)
       newMember.send(`You have been muted, reason - ${reason}`)
+    }
+    else if((firstEntry.target) && (JSON.stringify(oldFetchedLogs) !== JSON.stringify(fetchedLogs))){
+      sendLogEmbed({tag: "Automod", id: "Discord"}, firstEntry.target, "Member mute", newMember.guild, "Check <#969948890616975360> for more info")
+      
     }
     oldFetchedLogs = fetchedLogs
   }
 })
 
-  
-client.login(process.env.DJS_TOKEN)
+client.login("OTYxNjMxMDc1MjI0OTQ0NzQx.G8_L_I.HALyQpoC0Qql4LJTCVk0bjAMLNEhgIBgnTXOiA")
